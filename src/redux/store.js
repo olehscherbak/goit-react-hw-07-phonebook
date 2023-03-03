c5cfbfe29637c2160b6e7e983a23fe6562a7434e
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2023-03-03 16:19:58 +0200

    untied from localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer, filterReducer } from './slices';
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
-const persistedContactsReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedContactsReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
