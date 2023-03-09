8b149780bf163c0da73d8cb1c0ecb828fd146f46
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-03-09 21:46:20 +0200

    changed some styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
+
+import { AiFillFolderAdd } from 'react-icons/ai';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './ContactForm.module.css';
@@ -29,35 +31,49 @@ export default function ContactForm() {
 
   return (
     <form onSubmit={handleSubmit} className={css.form}>
-      <label className={css.label}>
-        Name
-        <input
-          className={css.input}
-          type="text"
-          name="name"
-          autoComplete="off"
-          pattern="^[a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ]+(([' -][a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ ])?[a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
+      <div className={css.container}>
+        <div className={css.inputsWraper}>
+          <label className={css.label}>
+            Name
+            <input
+              className={css.input}
+              type="text"
+              name="name"
+              autoComplete="off"
+              pattern="^[a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ]+(([' -][a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ ])?[a-zA-Zа-яА-ЯЇїЄєІіҐґ'’ʼ]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
 
-      <label className={css.label}>
-        Number
-        <input
-          className={css.input}
-          type="tel"
-          name="number"
-          autoComplete="off"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </label>
+          <label className={css.label}>
+            Number
+            <input
+              className={css.input}
+              type="tel"
+              name="number"
+              autoComplete="off"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+        </div>
 
-      <button type="submit" className={css.addBtn}>
-        Add contact
-      </button>
+        <button type="submit" className={css.addBtn}>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}
+          >
+            <AiFillFolderAdd size="1.5em" color="#777" />
+            <span>Add </span>
+          </div>
+          <span>contact</span>
+        </button>
+      </div>
       <ToastContainer />
     </form>
   );
